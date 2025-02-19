e54433c9692b6ebe3fcd126c678ed32c592564d3
Andrea Manzi <andrea.manzi@regione.toscana.it>
2025-02-19 13:46:59 +0100

    Refactor

--- main.sh
+++ main.sh
@@ -1,53 +1,88 @@
 #!/bin/bash
 
-# Load .env file if it exists
+# Carica il file .env se esiste
 if [ -f .env ]; then
     source .env
 fi
 
-sudo ./clean.sh
+# Variabili importanti
+CN_SERVER="${CN_SERVER:-server}"
+SHARED_DATA_DIR="${SHARED_DATA_DIR:-./shared-data}"
+OUTPUT_DATA_DIR="${OUTPUT_DATA_DIR:-./output}"
 
-# Crea la root CA
-docker compose run --remove-orphans --rm root-ca /scripts/init-root-ca.sh && docker compose stop root-ca && docker compose rm -f root-ca
-docker compose run --remove-orphans --rm intermediate-ca /scripts/init-intermediate-ca.sh && docker compose stop intermediate-ca && docker compose rm -f intermediate-ca
+# Funzione per gestire gli errori
+handle_error() {
+    echo "Errore: $1" >&2
+    exit 1
+}
 
-# Cambia i permessi delle directory
-echo "Impostazione dei permessi..."
-sudo chown -R manzolo:manzolo ./shared-data
+# Funzione per eseguire un comando docker compose e gestire gli errori
+run_docker_compose() {
+    docker compose run --remove-orphans --rm "$@" || handle_error "Errore durante l'esecuzione di docker compose: $?"
+    docker compose stop "$1" && docker compose rm -f "$1"
+}
 
-# Copia la CSR della Intermediate CA dalla Intermediate CA alla Root CA
-cp ./shared-data/intermediate-ca/certs/intermediate-ca.csr.pem ./shared-data/root-ca/csr/
+# Funzione per copiare un file e gestire gli errori
+copy_file() {
+    cp "$1" "$2" || handle_error "Errore durante la copia del file: $?"
+}
 
-# Firma la CSR della Intermediate CA con la Root CA
-echo "Firma della CSR della Intermediate CA con la Root CA..."
+# Funzione per impostare i permessi
+set_permissions() {
+    echo "Impostazione dei permessi..."
+    sudo chown -R manzolo:manzolo "$SHARED_DATA_DIR" || handle_error "Errore durante l'impostazione dei permessi: $?"
+}
 
-docker compose run --remove-orphans --rm root-ca /scripts/sign-intermediate-ca-csr.sh
+# Pulisci l'ambiente
+sudo ./clean.sh || handle_error "Errore durante la pulizia"
+mkdir -p "$SHARED_DATA_DIR" || handle_error "Errore durante la creazione della directory"
+rm -rf $OUTPUT_DATA_DIR
+mkdir -p $OUTPUT_DATA_DIR
 
-# Cambia i permessi delle directory
-echo "Impostazione dei permessi..."
-sudo chown -R manzolo:manzolo ./shared-data
+# Crea la CA radice
+run_docker_compose root-ca /scripts/init-root-ca.sh
 
-# Copia il certificato della Intermediate CA nella sua directory
-echo "Copia del certificato della Intermediate CA..."
-cp ./shared-data/root-ca/certs/intermediate-ca.crt.pem ./shared-data/intermediate-ca/certs/
+# Crea la CA intermedia
+run_docker_compose intermediate-ca /scripts/init-intermediate-ca.sh
+
+# Imposta i permessi
+set_permissions
+
+# Firma la CSR della CA intermedia con la CA radice
+echo "Firma della CSR della CA intermedia con la CA radice..."
+copy_file "$SHARED_DATA_DIR/intermediate-ca/certs/intermediate-ca.csr.pem" "$SHARED_DATA_DIR/root-ca/csr/"
+run_docker_compose root-ca /scripts/sign-intermediate-ca-csr.sh
+
+# Copia il certificato della CA intermedia
+echo "Copia del certificato della CA intermedia..."
+copy_file "$SHARED_DATA_DIR/root-ca/certs/intermediate-ca.crt.pem" "$SHARED_DATA_DIR/intermediate-ca/certs/"
 
 # Crea la CSR del server
 echo "Creazione della CSR del server..."
-docker compose run --rm server-ca /scripts/init-server-ca.sh && docker compose stop server-ca && docker compose rm -f server-ca
+run_docker_compose server-ca /scripts/init-server-ca.sh
 
-# Copia la CSR del server nella directory della Intermediate CA
-echo "Copia della CSR del server nella Intermediate CA..."
-cp ./shared-data/server-ca/csr/${CN_SERVER}.csr.pem ./shared-data/intermediate-ca/csr/
+# Firma la CSR del server con la CA intermedia
+echo "Firma della CSR del server con la CA intermedia..."
+copy_file "$SHARED_DATA_DIR/server-ca/csr/${CN_SERVER}.csr.pem" "$SHARED_DATA_DIR/intermediate-ca/csr/"
+run_docker_compose intermediate-ca /scripts/sign-server-ca-csr.sh
 
-docker compose run --rm intermediate-ca /scripts/sign-server-ca-csr.sh && docker compose stop intermediate-ca && docker compose rm -f intermediate-ca
-
-# Copia il certificato del server nella sua directory
+# Copia il certificato del server
 echo "Copia del certificato del server..."
-cp ./shared-data/intermediate-ca/certs/${CN_SERVER}.crt.pem ./shared-data/server-ca/certs/
+copy_file "$SHARED_DATA_DIR/intermediate-ca/certs/${CN_SERVER}.crt.pem" "$SHARED_DATA_DIR/server-ca/certs/"
 
-# Cambia i permessi delle directory
-echo "Impostazione dei permessi..."
-sudo chown -R manzolo:manzolo ./shared-data
+# Copia i certificati nel folder output
+copy_file "$SHARED_DATA_DIR/root-ca/certs/root-ca.crt.pem" "$OUTPUT_DATA_DIR"
+copy_file "$SHARED_DATA_DIR/intermediate-ca/certs/intermediate-ca.crt.pem" "$OUTPUT_DATA_DIR"
+copy_file "$SHARED_DATA_DIR/server-ca/certs/${CN_SERVER}.crt.pem" "$OUTPUT_DATA_DIR"
+
+# Creazione della fullchain.pem
+cat "$OUTPUT_DATA_DIR/${CN_SERVER}.crt.pem" "$OUTPUT_DATA_DIR/intermediate-ca.crt.pem" "$OUTPUT_DATA_DIR/root-ca.crt.pem" > "$OUTPUT_DATA_DIR/fullchain.pem" || handle_error "Errore durante la creazione di fullchain.pem"
+
+# Creazione del file contenente solo le CA (root e intermediate)
+cat "$OUTPUT_DATA_DIR/root-ca.crt.pem" "$OUTPUT_DATA_DIR/intermediate-ca.crt.pem" > "$OUTPUT_DATA_DIR/ca.pem" || handle_error "Errore durante la creazione di ca.pem"
+
+# Imposta i permessi
+set_permissions
 
 echo "Processo completato con successo!"
 exit 0
